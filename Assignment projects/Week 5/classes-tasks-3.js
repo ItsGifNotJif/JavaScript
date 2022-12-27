279dfe858c1b9d11ba4dbb2ba98a80623cd22a75
It's Gif Not Jif <jdavindsale@gmail.com>
2022-12-27 10:57:00 +0200

    30 days of js functions 1

--- Assignment projects/Week 5/classes-tasks-3.js
+++ Assignment projects/Week 5/classes-tasks-3.js
@@ -1,6 +1,30 @@
 console.groupCollapsed('https://edabit.com/challenge/2FF7RKw9RLwc3EBY9');
 {
-  // ... code
+  class Challenge{
+    constructor(id, level){
+      this.id = id;
+      this.level = level;
+    }
+
+    points(){
+      if(this.level = "VE") return 5;
+      if(this.level = "EA") return 10;
+      if(this.level = "ME") return 20;
+      if(this.level = "HA") return 40;
+      if(this.level = "VH") return 80;
+      if(this.level = "EX") return 120;
+    }
+  }
+
+  class User{
+    constructor(name, xp, log){
+      this.name = name;
+      this.xp = xp;
+      this.log = log;
+    }
+  }
+
+  newSolvedChallenge()
 }
 console.groupEnd();
 
