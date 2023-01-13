fb7cf509078e8db25e5626a3b5ccbba7343e06fe
It's Gif Not Jif <jdavindsale@gmail.com>
2023-01-13 07:42:35 +0200

    pamoka 40 encapsulation update

--- week 4/pamoka 40/40 - oop - encapsulation/tasks/task.js
+++ week 4/pamoka 40/40 - oop - encapsulation/tasks/task.js
@@ -38,19 +38,56 @@
 }
 
 {
-  //New syntax 
+  //Old syntax part 2
 
 
   const isObjectLiteral = (val) => Object.getPrototypeOf(val) === Object.prototype
   class House {
-    address;
-    city;
+    #address;
+    #city;
     #owners;
 
     constructor(address, city, owners) {
-      this.address = address;
-      this.city = city;
-      this.#owners = owners;
+      this.#address = address;
+      this.setCity(city);
+      this.setOwners(owners);
+    }
+
+    setAddress(adr) {
+      if (typeof adr !== "string") {
+        console.error("Error: adress must be a valid string")
+      }
+      if (adr.length < 2) {
+        console.error("Error: adress must be longer than 2 characters")
+      }
+      if (adr.length > 32) {
+        console.error("Error: adress must be shorter than 32 characters")
+      }
+    }
+
+    getAddress() {
+      return this.#address
+    }
+
+    setCity(val) {
+      if (typeof val !== "string") {
+        console.error("Error: in House.setCity - newCity must be an Array");
+        return;
+      }
+      if (val.length < 2) {
+        console.error("Error: in House.setCity - newCity must be longer than 2 characters");
+        return;
+      }
+      if (val.length > 32) {
+        console.error("Error: in House.setCity - newCity must be shorter than 32 characters");
+        return;
+      }
+      //if all conditions are passed, return this result. Delete in future, only used now for studying purposes : 
+      console.log(`%cnewCity are accepted: \n\t ${val}`, "color: green")
+      this.#city = val;
+    }
+    getCity() {
+      return this.#city;
     }
     setOwners(newOwners) {
       //checks- "newOwners" must be an array, with at least 1 value that has to be a string
@@ -71,7 +108,6 @@
         return;
       }
 
-
       //If no mistakes are made, "newOwners" is correct and can be assigned
       console.log(`%cnewOwners are accepted: Welcome: \n\t ${newOwners.map(x => x.name).join("\n\t")}`, "color: green")
       this.#owners = newOwners
@@ -81,7 +117,6 @@
       return this.#owners;
     }
   }
-
 
   const house = new House('Kimano g. 7', 'Kaunas', [{ name: 'Blakas Kojinskas' }]);
   const houses = [
@@ -106,13 +141,140 @@
 
   console.group('2. Perrašykite HouseInstance.address savybės inkapsuliaciją naudojant naują sintaksę');
   {
-
+    house.setAddress(9)
+    house.setAddress("2")
+    house.setAddress("Kaunas")
   }
   console.groupEnd();
 
   console.group('3. Perrašykite HouseInstance.city savybės inkapsuliaciją naudojant naują sintaksę');
   {
+    house.setCity(7)
+    house.setCity("7")
+    house.setCity("bell")
+  }
+  console.groupEnd();
 
+}
+
+{
+  //new syntax 
+
+
+  const isObjectLiteral = (val) => Object.getPrototypeOf(val) === Object.prototype
+  class House {
+    #address;
+    #city;
+    #owners;
+
+    constructor(address, city, owners) {
+      this.#address = address;
+      this.city = city;
+      this.setOwners(owners);
+    }
+
+    setAddress(adr) {
+      if (typeof adr !== "string") {
+        console.error("Error: adress must be a valid string")
+      }
+      if (adr.length < 2) {
+        console.error("Error: adress must be longer than 2 characters")
+      }
+      if (adr.length > 32) {
+        console.error("Error: adress must be shorter than 32 characters")
+      }
+    }
+
+    getAddress() {
+      return this.#address
+    }
+
+    set city(val) {
+      if (typeof val !== "string") {
+        console.error("Error: in House.setCity - newCity must be an Array");
+        return;
+      }
+      if (val.length < 2) {
+        console.error("Error: in House.setCity - newCity must be longer than 2 characters");
+        return;
+      }
+      if (val.length > 32) {
+        console.error("Error: in House.setCity - newCity must be shorter than 32 characters");
+        return;
+      }
+      //if all conditions are passed, return this result. Delete in future, only used now for studying purposes : 
+      console.log(`%cnewCity are accepted: \n\t ${val}`, "color: green")
+      this.#city = val;
+    }
+    get city() {
+      return this.#city;
+    }
+
+
+
+    setOwners(newOwners) {
+      //checks- "newOwners" must be an array, with at least 1 value that has to be a string
+      if (!Array.isArray(newOwners)) {
+        console.error("Error: in House.setOwners - newOwners must be an Array");
+        return;
+      }
+      if (newOwners.length === 0) {
+        console.error("Error: in House.setOwners - newOwners must not be empty");
+        return;
+      }
+      if (!newOwners.every(isObjectLiteral)) {
+        console.error("Error: in House.setOwners. Every owner must be an object literal");
+        return;
+      }
+      if (!newOwners.every((owner) => typeof owner.name === "string" && owner.name.length > 1)) {
+        console.error("Error: in House.setOwners. Every owner must have a name longer than 1 character");
+        return;
+      }
+
+      //If no mistakes are made, "newOwners" is correct and can be assigned
+      console.log(`%cnewOwners are accepted: Welcome: \n\t ${newOwners.map(x => x.name).join("\n\t")}`, "color: green")
+      this.#owners = newOwners
+    }
+
+    getOwners() {
+      return this.#owners;
+    }
+  }
+
+  const house = new House('Kimano g. 7', 'Kaunas', [{ name: 'Blakas Kojinskas' }]);
+  const houses = [
+    new House('Kimano g. 7', 'Kaunas', [{ name: 'Blakas Kojinskas' }]),
+    new House('Aliejų g. 121', 'Kaunas', [{ name: 'Sensas Jautimantas' }, { name: 'Rekė Jautimantė' }]),
+    new House('Sustenų g. 7', 'Vilnius', [{ name: 'Groja Blanienė' }, { name: 'Fartas Blanys' }]),
+    new House('Varkatkų g. 7', 'Klaipėda', [{ name: 'Faryja Mirgaitė' }]),
+  ];
+
+  console.group('1. HouseInstance.owners savybės inkapsuliacija');
+  {
+    console.log(house.getOwners())
+    house.setOwners([7])
+    house.setOwners([])
+    house.setOwners([7, 9])
+    house.setOwners([{}, {}, {}])
+    house.setOwners([{ name: 8 }, { name: 8 }, { name: 8 }])
+    house.setOwners([{ name: "Ghost" }, { name: "John Price" }, { name: "Soap McTavish" }])
+    console.log(house.getOwners())
+  }
+  console.groupEnd();
+
+  console.group('2. Perrašykite HouseInstance.address savybės inkapsuliaciją naudojant naują sintaksę');
+  {
+    house.setAddress(9)
+    house.setAddress("2")
+    house.setAddress("Kaunas")
+  }
+  console.groupEnd();
+
+  console.group('3. Perrašykite HouseInstance.city savybės inkapsuliaciją naudojant naują sintaksę');
+  {
+    house.city = 7; //calling (set city(7){...})
+    house.city = "9";
+    house.city = "bell"
   }
   console.groupEnd();
 
