e84e207f196e491a343d5765faaf7b64b90871b5
It's Gif Not Jif <jdavindsale@gmail.com>
2023-01-15 07:11:25 +0200

    pamoka 49 DOM update

--- week 6 DOM/49 - Document Object Model/self work/main.js
+++ week 6 DOM/49 - Document Object Model/self work/main.js
@@ -1,1 +1,10 @@
 ///DOM --- Document Object Model
+const buttonPrank = document.querySelector(".js_prank_button");
+const jsResult = document.querySelector(".js_prank_result");
+
+
+const prankFunction = () => {
+    jsResult.innerHTML = "You got pranked"
+}
+
+buttonPrank.addEventListener("click", prankFunction);
