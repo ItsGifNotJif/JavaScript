ffb2d7d3bf6dfcadfbe82536eeaa186de0a1fcb3
It's Gif Not Jif <jdavindsale@gmail.com>
2023-01-23 12:37:08 +0200

    pamoka 53 update

--- week 6 DOM and TypeScript Intro/53 - promises/self-work/task.js
+++ week 6 DOM and TypeScript Intro/53 - promises/self-work/task.js
@@ -1,14 +1,17 @@
 const userSelect = document.querySelector("#user-select");
 const alertContainer = document.querySelector(".js-alertContainer");
-console.log(alertContainer)
+const todosTable = document.querySelector(".js_todos_table")
+const todosTableBody = document.querySelector("tbody")
+
 
 const renderUserOptions = (users) => {
-    const optionsString = users.map(({ username, name }) => `<option>${username} - ${name}</option>`).join("")
+    const optionsString = users.map(({ username, name, id }) => `<option value="${id}">${username} - ${name}</option>`).join("")
     userSelect.innerHTML += optionsString;
 }
 
 
 const displayError = (error) => {
+    alertContainer.style.display = "block";
     alertContainer.innerHTML = `
     <div class="alert alert-warning" role="alert">
                 <div>
@@ -22,7 +25,33 @@ const displayError = (error) => {
 }
 
 
+
 fetch("https://jsonplaceholder.typicode.com/users")
     .then((res) => res.json())
     .then(renderUserOptions)
     .catch(displayError)
+
+
+const renderTodosTable = (todos) => {
+    if (todosTable.classList.contains("d-none"))
+        todosTable.classList.remove("d-none")
+    const todosRows = todos.map(({ id, title, completed }) =>
+        `<tr>
+        <th scope="row">${id}</th>
+        <td>${title}</td>
+        <td>${completed ? "Completed" : "Not Completed"}</td>
+    </tr>`
+    )
+        .join("");
+
+    todosTableBody.innerHTML = todosRows
+}
+
+
+userSelect.addEventListener("change", (e) => {
+    const userId = e.target.value;
+    fetch(`https://jsonplaceholder.typicode.com/todos?userId=${userId}`)
+        .then((response) => response.json())
+        .then(renderTodosTable)
+        .catch(displayError)
+})
