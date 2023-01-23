e7f94e5ce051bdef9d3bc22642ec004cbf2aba02
It's Gif Not Jif <jdavindsale@gmail.com>
2023-01-23 11:51:28 +0200

    part 53 update

--- week 6 DOM and TypeScript Intro/53 - promises/self-work/task.js
+++ week 6 DOM and TypeScript Intro/53 - promises/self-work/task.js
@@ -1,5 +1,6 @@
 const userSelect = document.querySelector("#user-select");
-
+const alertContainer = document.querySelector(".js-alertContainer");
+console.log(alertContainer)
 
 const renderUserOptions = (users) => {
     const optionsString = users.map(({ username, name }) => `<option>${username} - ${name}</option>`).join("")
@@ -7,6 +8,21 @@ const renderUserOptions = (users) => {
 }
 
 
+const displayError = (error) => {
+    alertContainer.innerHTML = `
+    <div class="alert alert-warning" role="alert">
+                <div>
+                    ${error.message}
+                </div>
+                <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+            </div>`
+    setTimeout(() => {
+        alertContainer.innerHTML = null
+    }, 5000)
+}
+
+
 fetch("https://jsonplaceholder.typicode.com/users")
     .then((res) => res.json())
     .then(renderUserOptions)
+    .catch(displayError)
