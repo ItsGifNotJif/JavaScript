7959daf1d088288cde343e00e5eb7e201a5ac9d4
It's Gif Not Jif <jdavindsale@gmail.com>
2023-04-24 00:07:55 +0300

    part 53 classwork update

--- week 6 DOM and TypeScript Intro/53 - promises and async functions/repeat/promise tasks/main.js
+++ week 6 DOM and TypeScript Intro/53 - promises and async functions/repeat/promise tasks/main.js
@@ -9,26 +9,47 @@
     1. Download users
     2. Sort selection based on downloaded users
     3. Create and test an error in case of failure
+    4. Download table data
+    5. Display data in HTML based on user selected
 
-
+    C. Sorting tasks
+    1. Sort downloaded data by users selected
 */
 
-const userSelect = document.querySelector("#user-select");;
-const alertContainer = document.querySelector(".js-alert-container")
-
+const userSelect = document.querySelector("#user-select");
+const alertContainer = document.querySelector(".js-alert-container");
+const todosTable = document.querySelector(".js-todos-table");
+const todosTableBody = todosTable.querySelector("tbody");
 
 const renderUserOptions = (users) => {
-    const optionsStr = users.map(({ name, username }) => `<option>${username} - ${name}</option>`)
+    const optionsStr = users.map(({ name, username, id }) => `<option value="${id}">${username} - ${name}</option>`)
         .join('');
     userSelect.innerHTML += optionsStr;
 }
 
+//B.5
+const renderTodosTable = (todos) => {
+    if (todosTable.classList.contains("d-none"))
+        todosTable.classList.remove("d-none");
+    todosTableBody.innerHTML = '';
+    const todosRowsStr = todos.map(({ id, title, completed }) => `
+    <tr>
+        <th scope="row">${id}</th>
+                <td>${title}</td>
+        <td>${completed ? "Yes" : "No"}</td>
+        </tr>`)
+        .join('');
+
+    todosTableBody.innerHTML += todosRowsStr;
+}
+
 //B.3
 const displayError = (error) => {
+    console.dir(error);
     alertContainer.innerHTML = `
     <div class="alert alert-warning alert-dismissible fade show" role="alert">
-   <div>${error.message}</div>
-    <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+        <div>${error.message}</div>
+        <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
     </div>`;
     setTimeout(() => {
         alertContainer.innerHTML = null;
@@ -41,4 +62,14 @@ fetch("https://jsonplaceholder.typicode.com/users")
     .then((res) => res.json())
     .then(renderUserOptions)
     //B.3
-    .catch(displayError)
+    .catch(displayError);
+
+
+
+userSelect.addEventListener('change', (e) => {
+    const userId = e.target.value;
+    fetch(`https://jsonplaceholder.typicode.com/todos?userId=${userId}`)
+        .then((res) => res.json())
+        .then(renderTodosTable)
+        .catch(displayError)
+})
