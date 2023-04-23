57ab38ff2408dd63df10b9098703ca22b3e322d3
It's Gif Not Jif <jdavindsale@gmail.com>
2023-04-23 18:16:21 +0300

    part 53 class work update

--- week 6 DOM and TypeScript Intro/53 - promises and async functions/repeat/promise tasks/main.js
+++ week 6 DOM and TypeScript Intro/53 - promises and async functions/repeat/promise tasks/main.js
@@ -5,27 +5,40 @@
     1. Create UI with a selection for the data we downlaod
     2. A selection of users
 
-    B. Downlaod tasks
+    B. Download tasks
     1. Download users
-    2. Sort selection based on downloaded users 
+    2. Sort selection based on downloaded users
     3. Create and test an error in case of failure
 
 
 */
 
-const userSelect = document.querySelector("#user-select");
+const userSelect = document.querySelector("#user-select");;
+const alertContainer = document.querySelector(".js-alert-container")
 
-console.log(userSelect)
 
 const renderUserOptions = (users) => {
-    const optionsStr = users.map(({ name, username }) => `<option>${username} - ${name}</option>`).join('');
+    const optionsStr = users.map(({ name, username }) => `<option>${username} - ${name}</option>`)
+        .join('');
     userSelect.innerHTML += optionsStr;
 }
 
-
+//B.3
+const displayError = (error) => {
+    alertContainer.innerHTML = `
+    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+   <div>${error.message}</div>
+    <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+    </div>`;
+    setTimeout(() => {
+        alertContainer.innerHTML = null;
+    }, 3500)
+}
 
 
 //B.1
 fetch("https://jsonplaceholder.typicode.com/users")
     .then((res) => res.json())
     .then(renderUserOptions)
+    //B.3
+    .catch(displayError)
---
+++ week 6 DOM and TypeScript Intro/53 - promises and async functions/repeat/promise tasks/main2.js
@@ -0,0 +1,11 @@
+// const userSelect = document.querySelector("#user-select")
+
+// const renderUserOptions = (users) => {
+//     const optionsStrings = users.map(({ name, username }) => `<option>${name} - ${username}</option>`)
+//         .join(" ");
+//     userSelect.innerHTML += optionsStrings;
+// }
+
+// fetch("https://jsonplaceholder.typicode.com/users")
+//     .then((response) => response.json())
+//     .then(renderUserOptions)
